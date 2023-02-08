cb8dbdfc9338e92b1c76325d865f98348a7be83c
Lucas Pinheiro Ribeiro <llucasp.ribeiro@hotmail.com>
2023-02-07 22:35:03 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,10 +1,26 @@
 package application;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
-    public static void printBoard(ChessPiece[][] pieces){
+    public static ChessPosition readChessPosition(Scanner scanner) {
+        try {
+            String s = scanner.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+        }
+    }
+
+
+    public static void printBoard(ChessPiece[][] pieces) {
 
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -16,10 +32,10 @@ public class UI {
         System.out.println("  a b c d e f g h");
     }
 
-    private static void printPiece(ChessPiece piece){
-        if (piece == null){
+    private static void printPiece(ChessPiece piece) {
+        if (piece == null) {
             System.out.print("-");
-        }else {
+        } else {
             System.out.print(piece);
         }
 
